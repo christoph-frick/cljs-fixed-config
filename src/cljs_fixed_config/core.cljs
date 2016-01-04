ad2ce34e74c341c223b50f156454d6274396df48
Christoph Frick <rid@ofnir.net>
2016-01-04 08:19:33 +0100

    add example for EDN

--- src/cljs_fixed_config/core.cljs
+++ src/cljs_fixed_config/core.cljs
@@ -1,4 +1,12 @@
 (ns cljs-fixed-config.core
   (:require-macros [cljs-fixed-config.core :as m]))
 
-(js/console.log (js/JSON.parse (m/load-config "test.json")))
+(def test-json 
+  (js/JSON.parse (m/load-config "test.json")))
+
+(js/console.log "JSON" (aget test-json "text"))
+
+(def test-edn 
+  (cljs.reader/read-string (m/load-config "test.edn")))
+
+(js/console.log "EDN" (:text test-edn))
